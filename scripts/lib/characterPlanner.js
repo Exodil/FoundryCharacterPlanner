02ddd6c4fe7c4893f0b72f49825b58f77efcc5d3
tomz <tomzangerl@gmail.com>
2020-10-27 16:13:12 +0100

    scene control button added

---
+++ scripts/lib/characterPlanner.js
@@ -0,0 +1,10 @@
+class CharacterPlanner{
+    
+    static async init(){
+        console.log("ZE CP IS INITINGING")
+    }
+
+    static async ready(){
+        console.log("ZE CP IS NOW READY")
+    }
+}
--- scripts/module.js
+++ scripts/module.js
@@ -1,37 +1,28 @@
-(() => { })();
 
 
-static getSceneControlButtons(buttons) {
-    //Hooked on the left-hand set of buttons; add a Create Quick Encounter one
-    let notesButton = buttons.find(b => b.name === "token");
+function getSceneControlButtons(buttons) {
+    let tokenButton = buttons.find(b => b.name == "token")
 
-    if (notesButton && game.user.isGM) {
-        notesButton.tools.push({
-            name: "linkEncounter",
-            title: game.i18n.localize("QE.CreateQuickEncounter.BUTTON"),
-            icon: "fas fa-fist-raised",
-            toggle: false,
-            button: true,
+    if (tokenButton) {
+        tokenButton.tools.push({
+            name: "char-planner",
+            title: "Character planner",
+            icon: "fas fa-atom",
             visible: game.user.isGM,
-            onClick: () => QuickEncounter.createOrRun()
-        });
-        notesButton.tools.push({
-            name: "deleteEncounters",
-            title: "Delete all Quick Encounter Map Notes",
-            icon: "fas fa-trash",
-            toggle: false,
-            button: true,
-            visible: false, //game.user.isGM,
-            onClick: () => QuickEncounter.deleteAllEQMapNotes("Unknown")
+            onClick: () => console.log("clicked"),
+            button: true
         });
     }
 }
 
 
-Hooks.once('init', async function() {
 
+Hooks.once('init', async function() {
+console.log("its initing")
 });
 
 Hooks.once('ready', async function() {
-
+console.log("its readying")
 });
+
+Hooks.on('getSceneControlButtons', getSceneControlButtons)
